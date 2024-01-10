mattermost/mattermost

MM-55468 Ensure custom status emojis exist (#25501)
* MM-55468 Ensure custom status emojis exist

* Fix plugin API unit test

* Print underlying error as detailed error message

* Convert CustomStatusModal tests to React Testing Library and improve a11y

* Don't suggest custom statuses with non-existent emojis

* Silence test error by providing fake translation strings

webapp/channels/src/components/custom_status/custom_status_modal.test.tsx
@@ -1,46 +1,156 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
-import {Provider} from 'react-redux';
+import type {AutoSizerProps} from 'react-virtualized-auto-sizer';
 
-import * as StatusSelectors from 'selectors/views/custom_status';
+import type {DeepPartial} from '@mattermost/types/utilities';
 
-import mockStore from 'tests/test_store';
+import {Preferences} from 'mattermost-redux/constants';
+
+import mergeObjects from 'packages/mattermost-redux/test/merge_objects';
+import {act, renderWithContext, screen, userEvent} from 'tests/react_testing_utils';
+import {TestHelper} from 'utils/test_helper';
+
+import type {GlobalState} from 'types/store';
 
 import CustomStatusModal from './custom_status_modal';
 
-jest.mock('selectors/views/custom_status');
+jest.mock('react-virtualized-auto-sizer', () => (props: AutoSizerProps) => props.children({height: 100, width: 100}));
+jest.mock('images/img_trans.gif', () => 'img_trans.gif');
 
-describe('components/custom_status/custom_status_modal', () => {
-    const store = mockStore({});
+describe('CustomStatusModal', () => {
     const baseProps = {
         onExited: jest.fn(),
     };
 
-    it('should match snapshot', () => {
-        const wrapper = shallow(
-            <Provider store={store}>
-                <CustomStatusModal {...baseProps}/>
-            </Provider>,
+    const initialState: DeepPartial<GlobalState> = {
+        entities: {
+            general: {
+                config: {
+                    EnableCustomEmoji: 'true',
+                    EnableCustomUserStatuses: 'true',
+                },
+            },
+        },
+    };
+
+    // The emoji picker renders emoji categories without passing a defaultMessage, and we don't pass translation strings
+    // into the provider by default, so we need to pass something for this string to silence errors from FormatJS.
+    const renderOptions = {
+        intlMessages: {
+            'emoji_picker.smileys-emotion': 'Smileys & Emotions',
+        },
+    };
+
+    test('should render suggested statuses until the user starts typing', () => {
+        renderWithContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            initialState,
+            renderOptions,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Out for lunch')).toBeInTheDocument();
+        expect(screen.getByLabelText(':hamburger:')).toBeInTheDocument();
+
+        userEvent.type(screen.getByPlaceholderText('Set a status'), 'Test status, please ignore');
+
+        expect(screen.queryByText('SUGGESTIONS')).not.toBeInTheDocument();
+        expect(screen.queryByText('Out for lunch')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':hamburger:')).not.toBeInTheDocument();
     });
 
-    it('should match snapshot when user has custom status set', () => {
-        const customStatus = {
-            emoji: 'speech_balloon',
-            text: 'speaking',
-        };
-        (StatusSelectors.makeGetCustomStatus as jest.Mock).mockReturnValue(() => customStatus);
-        const wrapper = shallow(
-            <Provider store={store}>
-                <CustomStatusModal {...baseProps}/>
-            </Provider>,
+    test('should render suggested statuses until the user selects an emoji', () => {
+        renderWithContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            initialState,
+            renderOptions,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByLabelText(':hamburger:')).toBeInTheDocument();
+        expect(screen.getByText('Out for lunch')).toBeInTheDocument();
+
+        userEvent.click(screen.getByLabelText('select an emoji'));
+        act(() => userEvent.click(screen.getByLabelText('grinning emoji')));
+
+        expect(screen.queryByText('SUGGESTIONS')).not.toBeInTheDocument();
+        expect(screen.queryByText('Out for lunch')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':hamburger:')).not.toBeInTheDocument();
+    });
+
+    test('should render recently used statuses as suggestions', () => {
+        const testState = mergeObjects(initialState, {
+            entities: {
+                preferences: {
+                    myPreferences: TestHelper.getPreferencesMock([
+                        {
+                            category: Preferences.CATEGORY_CUSTOM_STATUS,
+                            name: Preferences.NAME_RECENT_CUSTOM_STATUSES,
+                            value: JSON.stringify([
+                                TestHelper.getCustomStatusMock({emoji: 'taco', text: 'Eating a taco'}),
+                            ]),
+                        },
+                    ]),
+                },
+            },
+        });
+
+        renderWithContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            testState,
+            renderOptions,
+        );
+
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Eating a taco')).toBeInTheDocument();
+        expect(screen.getByLabelText(':taco:')).toBeInTheDocument();
+    });
+
+    test('should render recently used statuses with custom emojis which exist', () => {
+        const existentEmoji = TestHelper.getCustomEmojiMock({name: 'existent'});
+
+        const testState = mergeObjects(initialState, {
+            entities: {
+                emojis: {
+                    customEmoji: {
+                        [existentEmoji.id]: existentEmoji,
+                    },
+                },
+                preferences: {
+                    myPreferences: TestHelper.getPreferencesMock([
+                        {
+                            category: Preferences.CATEGORY_CUSTOM_STATUS,
+                            name: Preferences.NAME_RECENT_CUSTOM_STATUSES,
+                            value: JSON.stringify([
+                                TestHelper.getCustomStatusMock({emoji: 'existent', text: 'Existing'}),
+                                TestHelper.getCustomStatusMock({emoji: 'nonexistent', text: 'Not existing'}),
+                            ]),
+                        },
+                    ]),
+                },
+            },
+        });
+
+        renderWithContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            testState,
+            renderOptions,
+        );
+
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Existing')).toBeInTheDocument();
+        expect(screen.getByLabelText(':existent:')).toBeInTheDocument();
+        expect(screen.queryByText('Not existing')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':nonexistent:')).not.toBeInTheDocument();
     });
 });

hanzoai/team

webapp/channels/src/components/custom_status/custom_status_modal.test.tsx
@@ -1,46 +1,158 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
-import {Provider} from 'react-redux';
+import type {AutoSizerProps} from 'react-virtualized-auto-sizer';
 
-import * as StatusSelectors from 'selectors/views/custom_status';
+import type {DeepPartial} from '@mattermost/types/utilities';
 
-import mockStore from 'tests/test_store';
+import {Preferences} from 'mattermost-redux/constants';
+
+import mergeObjects from 'packages/mattermost-redux/test/merge_objects';
+import {act, renderWithFullContext, screen, userEvent} from 'tests/react_testing_utils';
+import {TestHelper} from 'utils/test_helper';
+
+import type {GlobalState} from 'types/store';
 
 import CustomStatusModal from './custom_status_modal';
 
-jest.mock('selectors/views/custom_status');
+jest.mock('react-virtualized-auto-sizer', () => (props: AutoSizerProps) => props.children({height: 100, width: 100}));
+jest.mock('images/img_trans.gif', () => 'img_trans.gif');
 
-describe('components/custom_status/custom_status_modal', () => {
-    const store = mockStore({});
+describe('CustomStatusModal', () => {
     const baseProps = {
         onExited: jest.fn(),
     };
 
-    it('should match snapshot', () => {
-        const wrapper = shallow(
-            <Provider store={store}>
-                <CustomStatusModal {...baseProps}/>
-            </Provider>,
+    const initialState: DeepPartial<GlobalState> = {
+        entities: {
+            general: {
+                config: {
+                    EnableCustomEmoji: 'true',
+                    EnableCustomUserStatuses: 'true',
+                },
+            },
+        },
+    };
+
+    // The emoji picker renders emoji categories without passing a defaultMessage, and we don't pass translation strings
+    // into the provider by default, so we need to pass something for this string to silence errors from FormatJS.
+    const intlMessages = {
+        'emoji_picker.smileys-emotion': 'Smileys & Emotions',
+    };
+
+    test('should render suggested statuses until the user starts typing', () => {
+        renderWithFullContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            initialState,
+            'en',
+            intlMessages,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Out for lunch')).toBeInTheDocument();
+        expect(screen.getByLabelText(':hamburger:')).toBeInTheDocument();
+
+        userEvent.type(screen.getByPlaceholderText('Set a status'), 'Test status, please ignore');
+
+        expect(screen.queryByText('SUGGESTIONS')).not.toBeInTheDocument();
+        expect(screen.queryByText('Out for lunch')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':hamburger:')).not.toBeInTheDocument();
     });
 
-    it('should match snapshot when user has custom status set', () => {
-        const customStatus = {
-            emoji: 'speech_balloon',
-            text: 'speaking',
-        };
-        (StatusSelectors.makeGetCustomStatus as jest.Mock).mockReturnValue(() => customStatus);
-        const wrapper = shallow(
-            <Provider store={store}>
-                <CustomStatusModal {...baseProps}/>
-            </Provider>,
+    test('should render suggested statuses until the user selects an emoji', () => {
+        renderWithFullContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            initialState,
+            'en',
+            intlMessages,
         );
 
-        expect(wrapper).toMatchSnapshot();
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByLabelText(':hamburger:')).toBeInTheDocument();
+        expect(screen.getByText('Out for lunch')).toBeInTheDocument();
+
+        userEvent.click(screen.getByLabelText('select an emoji'));
+        act(() => userEvent.click(screen.getByLabelText('grinning emoji')));
+
+        expect(screen.queryByText('SUGGESTIONS')).not.toBeInTheDocument();
+        expect(screen.queryByText('Out for lunch')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':hamburger:')).not.toBeInTheDocument();
+    });
+
+    test('should render recently used statuses as suggestions', () => {
+        const testState = mergeObjects(initialState, {
+            entities: {
+                preferences: {
+                    myPreferences: TestHelper.getPreferencesMock([
+                        {
+                            category: Preferences.CATEGORY_CUSTOM_STATUS,
+                            name: Preferences.NAME_RECENT_CUSTOM_STATUSES,
+                            value: JSON.stringify([
+                                TestHelper.getCustomStatusMock({emoji: 'taco', text: 'Eating a taco'}),
+                            ]),
+                        },
+                    ]),
+                },
+            },
+        });
+
+        renderWithFullContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            testState,
+            'en',
+            intlMessages,
+        );
+
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Eating a taco')).toBeInTheDocument();
+        expect(screen.getByLabelText(':taco:')).toBeInTheDocument();
+    });
+
+    test('should render recently used statuses with custom emojis which exist', () => {
+        const existentEmoji = TestHelper.getCustomEmojiMock({name: 'existent'});
+
+        const testState = mergeObjects(initialState, {
+            entities: {
+                emojis: {
+                    customEmoji: {
+                        [existentEmoji.id]: existentEmoji,
+                    },
+                },
+                preferences: {
+                    myPreferences: TestHelper.getPreferencesMock([
+                        {
+                            category: Preferences.CATEGORY_CUSTOM_STATUS,
+                            name: Preferences.NAME_RECENT_CUSTOM_STATUSES,
+                            value: JSON.stringify([
+                                TestHelper.getCustomStatusMock({emoji: 'existent', text: 'Existing'}),
+                                TestHelper.getCustomStatusMock({emoji: 'nonexistent', text: 'Not existing'}),
+                            ]),
+                        },
+                    ]),
+                },
+            },
+        });
+
+        renderWithFullContext(
+            <CustomStatusModal
+                {...baseProps}
+            />,
+            testState,
+            'en',
+            intlMessages,
+        );
+
+        expect(screen.getByText('SUGGESTIONS')).toBeInTheDocument();
+        expect(screen.getByText('Existing')).toBeInTheDocument();
+        expect(screen.getByLabelText(':existent:')).toBeInTheDocument();
+        expect(screen.queryByText('Not existing')).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(':nonexistent:')).not.toBeInTheDocument();
     });
 });