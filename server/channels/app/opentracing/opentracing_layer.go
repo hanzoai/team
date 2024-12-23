ebuildy/mattermost

Adds Custom Profile Attributes endpoints and app layer

server/channels/app/opentracing/opentracing_layer.go
@@ -2041,6 +2041,28 @@ func (a *OpenTracingAppLayer) CreateBot(rctx request.CTX, bot *model.Bot) (*mode
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) CreateCPAField(field *model.PropertyField) (*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.CreateCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.CreateCPAField(field)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) CreateChannel(c request.CTX, channel *model.Channel, addMember bool) (*model.Channel, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.CreateChannel")
@@ -3197,6 +3219,28 @@ func (a *OpenTracingAppLayer) DeleteBrandImage(rctx request.CTX) *model.AppError
 
 	defer span.Finish()
 	resultVar0 := a.app.DeleteBrandImage(rctx)
+
+	if resultVar0 != nil {
+		span.LogFields(spanlog.Error(resultVar0))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0
+}
+
+func (a *OpenTracingAppLayer) DeleteCPAField(id string) *model.AppError {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.DeleteCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0 := a.app.DeleteCPAField(id)
 
 	if resultVar0 != nil {
 		span.LogFields(spanlog.Error(resultVar0))
@@ -12677,6 +12721,28 @@ func (a *OpenTracingAppLayer) ListAutocompleteCommands(teamID string, T i18n.Tra
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) ListCPAFields() ([]*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.ListCPAFields")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.ListCPAFields()
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) ListDirectory(path string) ([]string, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.ListDirectory")
@@ -13358,6 +13424,28 @@ func (a *OpenTracingAppLayer) PatchBot(rctx request.CTX, botUserId string, botPa
 
 	defer span.Finish()
 	resultVar0, resultVar1 := a.app.PatchBot(rctx, botUserId, botPatch)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
+func (a *OpenTracingAppLayer) PatchCPAField(id string, patch *model.PropertyFieldPatch) (*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.PatchCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.PatchCPAField(id, patch)
 
 	if resultVar1 != nil {
 		span.LogFields(spanlog.Error(resultVar1))

server/public/model/property_field.go
@@ -29,6 +29,21 @@ type PropertyField struct {
 	DeleteAt   int64             `json:"delete_at"`
 }
 
+func (pf *PropertyField) Auditable() map[string]interface{} {
+	return map[string]interface{}{
+		"id":          pf.ID,
+		"group_id":    pf.GroupID,
+		"name":        pf.Name,
+		"attrs":       pf.Attrs,
+		"type":        pf.Type,
+		"target_id":   pf.TargetID,
+		"target_type": pf.TargetType,
+		"create_at":   pf.CreateAt,
+		"update_at":   pf.UpdateAt,
+		"delete_at":   pf.DeleteAt,
+	}
+}
+
 func (pf *PropertyField) PreSave() {
 	if pf.ID == "" {
 		pf.ID = NewId()
@@ -66,6 +81,36 @@ func (pf *PropertyField) IsValid() error {
 	}
 
 	return nil
+}
+
+type PropertyFieldPatch struct {
+	Name       *string            `json:"name"`
+	Type       *PropertyFieldType `json:"type"`
+	Attrs      *map[string]any    `json:"attrs"`
+	TargetID   *string            `json:"target_id"`
+	TargetType *string            `json:"target_type"`
+}
+
+func (f *PropertyField) Patch(patch *PropertyFieldPatch) {
+	if patch.Name != nil {
+		f.Name = *patch.Name
+	}
+
+	if patch.Type != nil {
+		f.Type = *patch.Type
+	}
+
+	if patch.Attrs != nil {
+		f.Attrs = *patch.Attrs
+	}
+
+	if patch.TargetID != nil {
+		f.TargetID = *patch.TargetID
+	}
+
+	if patch.TargetType != nil {
+		f.TargetType = *patch.TargetType
+	}
 }
 
 type PropertyFieldSearchOpts struct {

hanzoai/team

server/channels/app/opentracing/opentracing_layer.go
@@ -2041,6 +2041,28 @@ func (a *OpenTracingAppLayer) CreateBot(rctx request.CTX, bot *model.Bot) (*mode
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) CreateCPAField(field *model.PropertyField) (*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.CreateCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.CreateCPAField(field)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) CreateChannel(c request.CTX, channel *model.Channel, addMember bool) (*model.Channel, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.CreateChannel")
@@ -3197,6 +3219,28 @@ func (a *OpenTracingAppLayer) DeleteBrandImage(rctx request.CTX) *model.AppError
 
 	defer span.Finish()
 	resultVar0 := a.app.DeleteBrandImage(rctx)
+
+	if resultVar0 != nil {
+		span.LogFields(spanlog.Error(resultVar0))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0
+}
+
+func (a *OpenTracingAppLayer) DeleteCPAField(id string) *model.AppError {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.DeleteCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0 := a.app.DeleteCPAField(id)
 
 	if resultVar0 != nil {
 		span.LogFields(spanlog.Error(resultVar0))
@@ -5475,6 +5519,28 @@ func (a *OpenTracingAppLayer) GetBulkReactionsForPosts(postIDs []string) (map[st
 
 	defer span.Finish()
 	resultVar0, resultVar1 := a.app.GetBulkReactionsForPosts(postIDs)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
+func (a *OpenTracingAppLayer) GetCPAField(fieldID string) (*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.GetCPAField(fieldID)
 
 	if resultVar1 != nil {
 		span.LogFields(spanlog.Error(resultVar1))
@@ -12677,6 +12743,28 @@ func (a *OpenTracingAppLayer) ListAutocompleteCommands(teamID string, T i18n.Tra
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) ListCPAFields() ([]*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.ListCPAFields")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.ListCPAFields()
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) ListDirectory(path string) ([]string, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.ListDirectory")
@@ -13358,6 +13446,28 @@ func (a *OpenTracingAppLayer) PatchBot(rctx request.CTX, botUserId string, botPa
 
 	defer span.Finish()
 	resultVar0, resultVar1 := a.app.PatchBot(rctx, botUserId, botPatch)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
+func (a *OpenTracingAppLayer) PatchCPAField(fieldID string, patch *model.PropertyFieldPatch) (*model.PropertyField, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.PatchCPAField")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.PatchCPAField(fieldID, patch)
 
 	if resultVar1 != nil {
 		span.LogFields(spanlog.Error(resultVar1))

server/public/model/property_field.go
@@ -29,6 +29,21 @@ type PropertyField struct {
 	DeleteAt   int64             `json:"delete_at"`
 }
 
+func (pf *PropertyField) Auditable() map[string]interface{} {
+	return map[string]interface{}{
+		"id":          pf.ID,
+		"group_id":    pf.GroupID,
+		"name":        pf.Name,
+		"type":        pf.Type,
+		"attrs":       pf.Attrs,
+		"target_id":   pf.TargetID,
+		"target_type": pf.TargetType,
+		"create_at":   pf.CreateAt,
+		"update_at":   pf.UpdateAt,
+		"delete_at":   pf.DeleteAt,
+	}
+}
+
 func (pf *PropertyField) PreSave() {
 	if pf.ID == "" {
 		pf.ID = NewId()
@@ -66,6 +81,46 @@ func (pf *PropertyField) IsValid() error {
 	}
 
 	return nil
+}
+
+type PropertyFieldPatch struct {
+	Name       *string            `json:"name"`
+	Type       *PropertyFieldType `json:"type"`
+	Attrs      *map[string]any    `json:"attrs"`
+	TargetID   *string            `json:"target_id"`
+	TargetType *string            `json:"target_type"`
+}
+
+func (pfp *PropertyFieldPatch) Auditable() map[string]interface{} {
+	return map[string]interface{}{
+		"name":        pfp.Name,
+		"type":        pfp.Type,
+		"attrs":       pfp.Attrs,
+		"target_id":   pfp.TargetID,
+		"target_type": pfp.TargetType,
+	}
+}
+
+func (pf *PropertyField) Patch(patch *PropertyFieldPatch) {
+	if patch.Name != nil {
+		pf.Name = *patch.Name
+	}
+
+	if patch.Type != nil {
+		pf.Type = *patch.Type
+	}
+
+	if patch.Attrs != nil {
+		pf.Attrs = *patch.Attrs
+	}
+
+	if patch.TargetID != nil {
+		pf.TargetID = *patch.TargetID
+	}
+
+	if patch.TargetType != nil {
+		pf.TargetType = *patch.TargetType
+	}
 }
 
 type PropertyFieldSearchOpts struct {